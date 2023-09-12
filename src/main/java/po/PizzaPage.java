0034a815383093448bc62db71f5c77e8258505a4
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 22:17:03 +0300

    add PizzaPage class and constructor with WebDriver as parameter

---
+++ src/main/java/po/PizzaPage.java
@@ -0,0 +1,11 @@
+package po;
+
+import org.openqa.selenium.WebDriver;
+
+public class PizzaPage {
+    WebDriver driver;
+
+    public PizzaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
