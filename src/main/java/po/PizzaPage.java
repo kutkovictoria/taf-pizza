88446d6064440db2b00d4b7ed9c6d95f34b15626
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 22:33:18 +0300

    add margaritaLink and clickMargaritaLink()

--- src/main/java/po/PizzaPage.java
+++ src/main/java/po/PizzaPage.java
@@ -1,11 +1,19 @@
 package po;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PizzaPage {
     WebDriver driver;
+    private String margaritaLink = "Пицца Маргарита";
 
     public PizzaPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void clickMargaritaLink() {
+        WebElement margaritaLinkElement = driver.findElement(By.xpath(margaritaLink));
+        margaritaLinkElement.click();
     }
 }
