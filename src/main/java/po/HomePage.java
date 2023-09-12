9d6471ad219fe1fc099754da1be9caf1ececf58d
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 22:04:48 +0300

    add pizzaLink locator

--- src/main/java/po/HomePage.java
+++ src/main/java/po/HomePage.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver;
+    private String pizzalink = "//a[@href='/catalog/picca']";
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 }
-
-
