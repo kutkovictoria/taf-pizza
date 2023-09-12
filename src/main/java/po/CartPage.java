973b4a38c854d0f419c2778b21bb19b1d14a3de8
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-13 00:39:44 +0300

    add margaritaCartLink and getMargaritaCartLink()

--- src/main/java/po/CartPage.java
+++ src/main/java/po/CartPage.java
@@ -1,11 +1,19 @@
 package po;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CartPage {
     WebDriver driver;
+    private String margaritaCartLink = "//*[@id='basket-el-0']/div[2]/div[1]/div[1]";
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public String getMargaritaCartLink() {
+        WebElement textMargaritaCartLinkElement = driver.findElement(By.xpath(margaritaCartLink));
+        return textMargaritaCartLinkElement.getText();
     }
 }
