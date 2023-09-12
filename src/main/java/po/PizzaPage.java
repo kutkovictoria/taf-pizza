9091c7272e382040c7e1b6f44cf672cbf0e0017f
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-13 00:27:42 +0300

    add CartLink and clickCartLink()

--- src/main/java/po/PizzaPage.java
+++ src/main/java/po/PizzaPage.java
@@ -3,21 +3,32 @@ package po;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PizzaPage {
     WebDriver driver;
     private String margaritaTypeLink = "//li[@data-id='589']";
     private String addMargaritaToCartLink = "//*[@id='menu-card-364']/div[3]/div[3]/div[2]/div[2]/button";
+    private String cartLink = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
 
     public PizzaPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void clickMargaritaTypeLink() {
-        WebElement margaritaTypeLinkElement = driver.findElement(By.xpath(margaritaTypeLink));
+        WebElement margaritaTypeLinkElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(margaritaTypeLink)));
     }
 
     public void clickAddMargaritaToCartLink() {
-        WebElement addMargaritaToCartLinkElement = driver.findElement(By.xpath(addMargaritaToCartLink));
+        WebElement addMargaritaToCartLinkElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(addMargaritaToCartLink)));
+    }
+    public void clickCurtLink() {
+        WebElement addMargaritaToCartLinkElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(cartLink)));
     }
 }
