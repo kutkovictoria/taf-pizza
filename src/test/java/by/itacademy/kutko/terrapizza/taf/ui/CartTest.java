2443cd71b62417eab1657c2a6ca0a64f43c40e93
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 23:56:28 +0300

    add testMargaritaAddedToCart()

--- src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
+++ src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
@@ -1,5 +1,17 @@
 package by.itacademy.kutko.terrapizza.taf.ui;
 
-public class CartTest extends BaseTest{
+import org.junit.jupiter.api.Test;
+import po.HomePage;
+import po.PizzaPage;
 
+public class CartTest extends BaseTest {
+    @Test
+    public void testMargaritaAddedToCart() {
+        HomePage homePage = new HomePage(driver);
+        PizzaPage pizzaPage = new PizzaPage(driver);
+        homePage.clickPizzaLink();
+        pizzaPage.clickMargaritaLink();
+        pizzaPage.clickMargaritaTypeLink();
+        pizzaPage.clickAddMargaritaToCartLink();
+    }
 }
