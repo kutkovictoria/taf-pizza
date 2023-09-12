56fd09acf4d9abfa6d5284434b3bb17ec02461a4
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 22:10:14 +0300

    add clickPizzaLink()

--- src/main/java/po/HomePage.java
+++ src/main/java/po/HomePage.java
@@ -1,11 +1,19 @@
 package po;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
     private WebDriver driver;
-    private String pizzalink = "//a[@href='/catalog/picca']";
+    private String pizzaLink = "//a[@href='/catalog/picca']";
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void clickPizzaLink() {
+        WebElement pizzaLinkElement = driver.findElement(By.xpath(pizzaLink));
+        pizzaLinkElement.click();
     }
 }
