2e9a8ca0612df3182710adfbdd72e79f2d6f5f11
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 21:51:21 +0300

    add driverSetup()

--- src/test/java/by/itacademy/kutko/terrapizza/taf/ui/BaseTest.java
+++ src/test/java/by/itacademy/kutko/terrapizza/taf/ui/BaseTest.java
@@ -1,4 +1,16 @@
 package by.itacademy.kutko.terrapizza.taf.ui;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+    WebDriver driver;
+    @BeforeEach
+    public void driverSetup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://terrapizza.by/");
+    }
 }
+
