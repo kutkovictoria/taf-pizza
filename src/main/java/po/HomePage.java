91ad9d55c51f2abac6720310ba145b566afcac8a
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-12 21:42:00 +0300

    add constructor with WebDriver as a parameter

--- src/main/java/po/HomePage.java
+++ src/main/java/po/HomePage.java
@@ -1,5 +1,12 @@
 package po;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    private WebDriver driver;
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
+
 
