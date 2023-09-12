a2856b00440733d01d1a7b369f80d8e52dbfacca
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-13 00:11:42 +0300

    delete margaritaLink and clickMargaritaLink from PizzaPage

--- src/main/java/po/PizzaPage.java
+++ src/main/java/po/PizzaPage.java
@@ -6,22 +6,18 @@ import org.openqa.selenium.WebElement;
 
 public class PizzaPage {
     WebDriver driver;
-    private String margaritaLink = "//div[@title='Пицца Маргарита']";
     private String margaritaTypeLink = "//li[@data-id='589']";
-    private String addMargaritaToCartLink = "//button[@data-id='364']";
+    private String addMargaritaToCartLink = "//*[@id='menu-card-364']/div[3]/div[3]/div[2]/div[2]/button";
 
     public PizzaPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void clickMargaritaLink() {
-        WebElement margaritaLinkElement = driver.findElement(By.xpath(margaritaLink));
-        margaritaLinkElement.click();
-    }
-    public void clickMargaritaTypeLink(){
+    public void clickMargaritaTypeLink() {
         WebElement margaritaTypeLinkElement = driver.findElement(By.xpath(margaritaTypeLink));
     }
-    public void clickAddMargaritaToCartLink(){
+
+    public void clickAddMargaritaToCartLink() {
         WebElement addMargaritaToCartLinkElement = driver.findElement(By.xpath(addMargaritaToCartLink));
     }
 }
