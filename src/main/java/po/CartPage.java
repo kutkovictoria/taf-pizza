95d4265f2bf534d10da22c46c236ce19480a7ea3
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-13 00:19:16 +0300

    add CartPage with constructor

---
+++ src/main/java/po/CartPage.java
@@ -0,0 +1,11 @@
+package po;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
