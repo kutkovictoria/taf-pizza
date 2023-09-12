36f409c1b6c40701fb346c65222c4aee2cb35312
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-13 00:48:06 +0300

    refactor CartTest

--- src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
+++ src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
@@ -1,6 +1,9 @@
 package by.itacademy.kutko.terrapizza.taf.ui;
 
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import po.CartPage;
 import po.HomePage;
 import po.PizzaPage;
 
@@ -9,9 +12,13 @@ public class CartTest extends BaseTest {
     public void testMargaritaAddedToCart() {
         HomePage homePage = new HomePage(driver);
         PizzaPage pizzaPage = new PizzaPage(driver);
+        CartPage cartPage = new CartPage(driver);
         homePage.clickPizzaLink();
-        pizzaPage.clickMargaritaLink();
-        pizzaPage.clickMargaritaTypeLink();
+        //pizzaPage.clickMargaritaTypeLink();
         pizzaPage.clickAddMargaritaToCartLink();
+        pizzaPage.clickCurtLink();
+        String expectedTextForPizzaInCart = "Пицца Маргарита";
+        String actualTextForPizzaInCart = cartPage.getMargaritaCartLink();
+        Assertions.assertEquals(expectedTextForPizzaInCart,actualTextForPizzaInCart);
     }
 }
