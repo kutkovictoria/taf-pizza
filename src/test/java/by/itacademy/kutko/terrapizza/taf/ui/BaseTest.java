f8f80d5150070104b4a50fa4ff0c746a78ac60a9
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-17 17:09:30 +0300

    refactor move ChromeDriver to Singleton class

--- src/test/java/by/itacademy/kutko/terrapizza/taf/ui/BaseTest.java
+++ src/test/java/by/itacademy/kutko/terrapizza/taf/ui/BaseTest.java
@@ -1,25 +1,21 @@
 package by.itacademy.kutko.terrapizza.taf.ui;
 
+import by.itacademy.kutko.terrapizza.taf.util.Singleton;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import po.HomePage;
+import by.itacademy.kutko.terrapizza.taf.po.HomePage;
 
 public class BaseTest {
-    WebDriver driver;
 
     @BeforeEach
     public void driverSetup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://terrapizza.by/");
-        HomePage homePage = new HomePage(driver);
+        HomePage homePage = new HomePage();
+        homePage.openHomePage();
         homePage.closeCookiesBanner();
     }
 
     @AfterEach
     public void driverShutDown() {
-        driver.quit();
+        Singleton.quitDriver();
     }
 }
