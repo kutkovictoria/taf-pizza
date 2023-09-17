1d46f242ea15d59157f524f16423901466de9866
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-17 17:10:25 +0300

    add selectMargaritaGrandType() and tryCatch

--- src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
+++ src/test/java/by/itacademy/kutko/terrapizza/taf/ui/CartTest.java
@@ -2,21 +2,29 @@ package by.itacademy.kutko.terrapizza.taf.ui;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import po.CartPage;
-import po.HomePage;
-import po.PizzaPage;
+import by.itacademy.kutko.terrapizza.taf.po.CartPage;
+import by.itacademy.kutko.terrapizza.taf.po.HomePage;
+import by.itacademy.kutko.terrapizza.taf.po.PizzaPage;
 
 public class CartTest extends BaseTest {
     @Test
     public void testMargaritaAddedToCart() {
-        HomePage homePage = new HomePage(driver);
-        PizzaPage pizzaPage = new PizzaPage(driver);
-        CartPage cartPage = new CartPage(driver);
+        HomePage homePage = new HomePage();
+        PizzaPage pizzaPage = new PizzaPage();
+        CartPage cartPage = new CartPage();
         homePage.openPizzaPage();
-        //pizzaPage.selectMargaritaType();
+        pizzaPage.selectMargaritaGrandType();
+        try {
+            Thread.sleep(300);
+        } catch (Exception e) {
+        }
         pizzaPage.addMargaritaToCart();
         pizzaPage.openCart();
-        String expectedTextForPizzaInCart = "Пицца Маргарита Классическая";
+        String expectedTextForPizzaInCart = "Пицца Маргарита Гранд 32 см";
+        try {
+            Thread.sleep(300);
+        } catch (Exception e) {
+        }
         String actualTextForPizzaInCart = cartPage.getMargaritaCartLink();
         Assertions.assertEquals(expectedTextForPizzaInCart, actualTextForPizzaInCart);
     }
